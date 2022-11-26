17afd27871d931bdff8033f19544c81d950525d6
nico-samuelson <nicos.t.65@gmail.com>
2022-11-26 17:25:52 +0700

    header buat navbar + basic style

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Max Heap Visualizer</title>
+    <?php include "header.php"; ?>
+</head>
+<body>
+    <p>Max Heap Visualizer</p>
+</body>
+</html>
