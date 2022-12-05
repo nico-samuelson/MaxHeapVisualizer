8303f9d5ccf2042b4fc6fdeeded9ffbf47ef45e4
MichaelAdiPratama <c14210016@john.petra.ac.id>
2022-12-05 20:00:11 +0700

    .

--- credits.php
+++ credits.php
@@ -5,7 +5,38 @@
 <!DOCTYPE html>
 <html>
     <header>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Credits - Kelompok 11 Struktur Data A</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script type="text/javascript" async="" src="https://www.gstatic.com/recaptcha/releases/Km9gKuG06He-isPsP6saG8cn/recaptcha__en.js" crossorigin="anonymous" integrity="sha384-tL/88+JE9Mv/pJzkZt5DBcS7efRA2LrT4YX0Htu8Rnmat69OIBwraynHJm/fWfbH"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+    <style media="screen">
+    .bg {
+      background-image: url('https://wgg.petra.ac.id/assets/background/main/web-malam-pc.jpg');
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: right;
+      height: -webkit-fill-available;
+      width: 100vw;
+      height: 100vh;
+      position: fixed;
+      z-index: -99;
 
+    }
+
+    @media only screen and (max-width: 768px) {
+      .bg {
+        background-image: url('https://wgg.petra.ac.id/assets/background/main/web-malam-smartphone.jpg');
+      }
+    }
+          h1,h2{
+        color: white;
+      }
+      </style>
     </header>
     <body>
     <div class="bg"></div>
