f7bad9dbb036fd942e74b83bb48c35405bff6951
MichaelAdiPratama <c14210016@john.petra.ac.id>
2022-12-06 10:30:16 +0700

    home deskripsi

--- home.php
+++ home.php
@@ -66,6 +66,22 @@
             background-color: orange;
             z-index: 5;
         } 
+        /* kolom */
+        .kolom {
+            margin-top: 20px;
+            margin-bottom: 20px;
+            margin-left: 20px;
+            margin-right: 20px;
+                }
+        /* kolom deskripsi/penjelasan */
+        .kolom .deskripsi {
+            font-size: 20px;
+            font-weight: bold;
+            font-family: 'comic sans ms';
+            color: #628395;
+            margin-left: 20px;
+            margin-right: 20px;
+        }
     </style>
 </head>
 <body style="background-color: cyan;background-image:url('')">
@@ -76,19 +92,62 @@
 
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="btn btn-primary me-6 mt-2 mb-2" href="index.html">Max Heap Tree Visualization</a>
-                    <a class="btn btn-success me-6 mt-2 mb-2" href="credits.php" style="margin-left: 20px;">Credits</a>
+                    <a class="btn btn-danger me-6 mt-2 mb-2" href="index.html">Max Heap Tree Visualization</a>
+                    <a class="btn btn-warning me-6 mt-2 mb-2" href="#deskripsi" style="margin-left: 10px;">Description of Heap Tree</a>
+                    <a class="btn btn-success me-6 mt-2 mb-2" href="#maxheap" style="margin-left: 10px;"> Max Heap Tree</a>
+                    <a class="btn btn-info me-6 mt-2 mb-2" href="#implementation" style="margin-left: 10px;"> Implementation Max Heap</a>
+                    <a class="btn btn-secondary me-6 mt-2 mb-2" href="credits.php" style="margin-left: 10px;">Credits</a>
                 </li>
             </ul>
         </div>
     </nav>
 
     <div class="container-fluid d-flex">
-        <div class="row pt-3 d-flex justify-content-center" style="width:100%" style="background-color: black;">
-           
-
+            <div class="row pt-3 d-flex justify-content-center" style="width:100%" style="background-color: black;">
             
-    </div>
+                <div class="wrapper">
+                    <!--untuk deskripsi-->
+                    <section id="deskripsi" >
+                        <div class="kolom">
+                        <h2 style="margin-left: 450px;"><strong>Description Of Heap Tree</strong></h2>
+                        <br>
+                        <p>A heap is a specialized tree-based data structure which is essentially an almost complete tree that satisfies the heap property: in a max heap, for any given node C, if P is a parent node of C, then the key (the value) of P is greater than or equal to the key of C. In a min heap, the key of P is less than or equal to the key of C.The node at the "top" of the heap (with no parents) is called the root node.</p>
+                        <br>
+                        <p>The heap is one maximally efficient implementation of an abstract data type called a priority queue, and in fact, priority queues are often referred to as "heaps", regardless of how they may be implemented. In a heap, the highest (or lowest) priority element is always stored at the root. However, a heap is not a sorted structure; it can be regarded as being partially ordered. A heap is a useful data structure when it is necessary to repeatedly remove the object with the highest (or lowest) priority, or when insertions need to be interspersed with removals of the root node.</p>
+                        <br>
+                        <p> A Heap is a special Tree-based data structure in which the tree is a complete binary tree. Since a heap is a complete binary tree, a heap with N nodes has log N height. It is useful to remove the highest or lowest priority element. It is typically represented as an array. There are two types of Heaps in the data structure.</p>
+                        <br>
+                        </div>
+                    </section>
+                    <!--untuk maxheap-->
+                    <section id="maxheap">
+                        <div class="kolom">
+                            
+                            <h2 style="margin-left: 500px;"><strong>Max Heap Tree</strong></h2>
+                            <br>
+                            <p> Selama situasi dan kondisi Pandemi, Seluruh kegiatan dan jadwal courses kami putuskan dalam bentuk online class. Tetapi jangan khawatir, Kita tetap akan memberikan members atau siswa kita dengan beragam fasilitas dan benefit!Hopefully our members can enjoy our courses.</p>
+                            <p> Kita juga memberikan beragam penawaran bagi rekan-rekan yang mengajak teammates atau saudara untuk bergabung dalam courses kami. Jadi, jangan lupa untuk mendaftar.. karena slots terbatas!
+                                See you in the class!</p>
+                               
+                        </div>
+                        <img src="" />
+                    </section>
+                    <!--untuk implementasi dari maxheap-->
+                    <section id="implementation">
+                        <div class="tengah">
+                            <div class="kolom">
+                                <br>
+                                <h2 style="margin-left: 400px;"><b>Implementation of Max Heap Tree</b></h2>
+                                <br>
+                                <p> Tutors kami telah berpengalaman mengajar di dalam bidang komputer science selama 15 tahun.
+                                    Jadi jangan khawatir mengenai kualitas pengajar kami. Kami juga bekerja sama dengan berbagai company baik di dalam ataupun di luar negeri untuk intership program.
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+    
+            </div>
 
 </body>
 </html>
