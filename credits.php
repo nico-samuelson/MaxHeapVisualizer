d24821d07e02b8eccf5e7e16075b28d21751f949
MichaelAdiPratama <c14210016@john.petra.ac.id>
2022-12-16 18:56:41 +0700

    design page visualization

--- credits.php
+++ credits.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Credits - Kelompok 11 Struktur Data A</title>
+    <title>Kelompok 11 Struktur Data A</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script type="text/javascript" async="" src="https://www.gstatic.com/recaptcha/releases/Km9gKuG06He-isPsP6saG8cn/recaptcha__en.js" crossorigin="anonymous" integrity="sha384-tL/88+JE9Mv/pJzkZt5DBcS7efRA2LrT4YX0Htu8Rnmat69OIBwraynHJm/fWfbH"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -115,15 +115,18 @@
 
         button:hover::before {
             width: 9em;
+            background-image: linear-gradient(to right, #0fd850 0%, #f9f047 100%);
         }
 
         button:hover {
             transform:scale(1.1);
+            background-color: transparent;
         }
         .navbar {
             background-color: transparent;
             transition: all .3s ease;
             backdrop-filter: blur(3px);
+            
         }
 
         .nav-link:hover {
@@ -137,7 +140,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="home.php" style="margin-left: 20px;">Credits</a>
+            <a class="navbar-brand" href="credits.php" style="margin-left: 20px;">Credits</a>
 
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li>
